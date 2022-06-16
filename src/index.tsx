7a2537337c0bd3a70d7a4f93768559f063906772
Artem Paskal <artem.yogi@gmail.com>
2022-06-16 11:09:34 +0300

    Weather Forecast

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import store, { persistor } from './store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor} >
         <App />
       </PersistGate>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
